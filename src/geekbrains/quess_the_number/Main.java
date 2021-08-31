8e7d9b4afeb9b9537317cb84ac29cf525dbdb7b6
valentin <delo.biss13@gmail.com>
2021-08-31 20:26:17 +0300

    Java.создали игру угадай число

--- src/geekbrains/quess_the_number/Main.java
+++ src/geekbrains/quess_the_number/Main.java
@@ -1,8 +1,31 @@
 package geekbrains.quess_the_number;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        //выводим надпись
+        System.out.println("Ваша задача угадать число.");
+        //задаем число
+        int range = 10;
+        //пишем чтоб программа рандомно выбирала загадаемое число до 10
+        int number = (int)(Math.random() * range);
+        while (true) {
+            System.out.println("Угадайте число от 0 до " + range);
+            int input_number = scanner.nextInt();
+            if (input_number == number) {
+                System.out.println("Вы угадали.");
+                break;
+            } else if (input_number > number) {
+                System.out.println("Загаданное число меньше.");
+            } else {
+                System.out.println("Загаданное число больше.");
+            }
+        }
+        scanner.close();
     }
+
 }
+
